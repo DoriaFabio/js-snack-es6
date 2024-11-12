43a8bb3366889edba38e16acf55dc237e10bdbf1
DoriaFabio <doriafabio2001@gmail.com>
2024-11-12 19:34:21 +0100

    do second exercise

--- script.js
+++ script.js
@@ -24,8 +24,8 @@ const bici = [
     }
 ];
 let biciLeggera = bici[0];
-for(let i = 1; i < bici.length; i++){
-    if(bici[i].peso < biciLeggera.peso) {
+for (let i = 1; i < bici.length; i++) {
+    if (bici[i].peso < biciLeggera.peso) {
         biciLeggera = bici[i];
     }
 }
@@ -41,55 +41,61 @@ console.log(biciLeggera);
 const footballTeam = [
     {
         nome: "Napoli",
-        puntiFatti: 0, 
+        puntiFatti: 0,
         falliSubiti: 0
     },
     {
         nome: "Juventus",
-        puntiFatti: 0, 
+        puntiFatti: 0,
         falliSubiti: 0
     },
     {
         nome: "Inter",
-        puntiFatti: 0, 
+        puntiFatti: 0,
         falliSubiti: 0
     },
     {
         nome: "Milan",
-        puntiFatti: 0, 
+        puntiFatti: 0,
         falliSubiti: 0
     },
     {
         nome: "Lazio",
-        puntiFatti: 0, 
+        puntiFatti: 0,
         falliSubiti: 0
     },
     {
         nome: "Roma",
-        puntiFatti: 0, 
+        puntiFatti: 0,
         falliSubiti: 0
     },
     {
         nome: "Atalanta",
-        puntiFatti: 0, 
+        puntiFatti: 0,
         falliSubiti: 0
     },
     {
         nome: "Fiorentina",
-        puntiFatti: 0, 
+        puntiFatti: 0,
         falliSubiti: 0
     }
 ];
 
-function random (min, max) {
-    return Math.floor(Math.random()*(max - min + 1)) + min;
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-for (let i = 0; i<footballTeam.length; i++) {
+for (let i = 0; i < footballTeam.length; i++) {
     footballTeam[i].puntiFatti = random(1, 100);
     footballTeam[i].falliSubiti = random(1, 50);
 }
 console.log(footballTeam);
 
+const newArray = footballTeam.map(Team => ({
+    nome: Team.nome,
+    falliSubiti: Team.falliSubiti
+  }));
+  
+  console.log(newArray);
 // Snack 3 (Bonus)
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
